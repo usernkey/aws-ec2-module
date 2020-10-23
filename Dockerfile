7fb527abc8711a361fbc3736ee03b8600107f699
Marco Giovannini <usernkey@gmail.com>
2020-10-24 00:24:51 +0200

    Basic Flask app

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,6 @@
 FROM python:3.7.9-alpine3.12
+COPY src/ /app/
+WORKDIR /app
+RUN pip3 install -r requirements.txt
+WORKDIR /app/api
+CMD [ "python", "api.py" ]
---
+++ src/api/api.py
@@ -0,0 +1,13 @@
+import flask
+from flask import request, jsonify
+import os
+
+app = flask.Flask(__name__)
+
+@app.route('/hello/', methods=['GET'])
+def hello():
+
+    return('Hello World')
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
