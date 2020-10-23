4e18b1449d2470350cc59682ce026bbe9eeabbb7
Marco Giovannini <usernkey@gmail.com>
2020-10-24 00:55:14 +0200

    syntax

--- src/api/api.py
+++ src/api/api.py
@@ -6,10 +6,9 @@ app = flask.Flask(__name__)
 @app.route('/hello/', methods=['GET'])
 def hello():
 
-
     return('Hello World')
 
-if __name__ == '__main__':
 
+if __name__ == '__main__':
 
     app.run(debug=True, host='0.0.0.0')
