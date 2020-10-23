65ac420c9c3af289af8986a39fb5e56564c0d75a
Marco Giovannini <usernkey@gmail.com>
2020-10-24 00:38:17 +0200

    unused and linter

--- src/api/api.py
+++ src/api/api.py
@@ -1,12 +1,13 @@
 import flask
-from flask import request
 
 app = flask.Flask(__name__)
 
 @app.route('/hello/', methods=['GET'])
+
 def hello():
 
     return('Hello World')
 
 if __name__ == '__main__':
+
     app.run(debug=True, host='0.0.0.0')
